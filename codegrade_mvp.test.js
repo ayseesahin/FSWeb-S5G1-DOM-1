ec0e5f88a69262f7098825072a0aacca97855372
Ayşe Nur Şahin <aysesah-95@hotmail.com>
2023-03-20 16:01:37 +0300

    finished

--- codegrade_mvp.test.js
+++ codegrade_mvp.test.js
@@ -1,9 +1,9 @@
-const { server } = require('./mocks/server')
-const { screen } = require('@testing-library/dom')
-require('@testing-library/jest-dom/extend-expect')
+const { server } = require("./mocks/server");
+const { screen } = require("@testing-library/dom");
+require("@testing-library/jest-dom/extend-expect");
 
 beforeAll(() => {
-  server.listen()
+  server.listen();
   document.body.innerHTML = `
     <div class="container">
     <header>
@@ -61,132 +61,151 @@ beforeAll(() => {
       <a href="#"></a>
     </footer>
   </div>
-  `
-  require('./src/index.js')
-})
+  `;
+  require("./src/index.js");
+});
 afterAll(() => {
-  server.close()
-})
+  server.close();
+});
 afterEach(() => {
-  server.resetHandlers()
-})
+  server.resetHandlers();
+});
 
-describe('metin içerikleri', () => {
-  test('[1] header menü linkleri doğru kelimeleri içeriyor', () => {
-    expect(screen.getByText('Servisler', { selector: 'nav a' }))
-    expect(screen.getByText('Ürünler', { selector: 'nav a' }))
-    expect(screen.getByText('Vizyon', { selector: 'nav a' }))
-    expect(screen.getByText('Özellikler', { selector: 'nav a' }))
-    expect(screen.getByText('Hakkımızda', { selector: 'nav a' }))
-    expect(screen.getByText('İletişim', { selector: 'nav a' }))
-  })
-  test('[2] cta elementleri doğru kelimeleri içeriyor', () => {
-    expect(screen.getByText('Bu DOM Mükemmel'))
-    expect(screen.getByText('Başlayın'))
-  })
-  test('[3] ana içerik başlığı doğru kelimeyi içeriyor', () => {
-    expect(screen.getByText(
-      'Özellikler',
-      { selector: '.top-content .text-content:nth-of-type(1) h4' }
-    ))
-    expect(screen.getByText(
-      'Hakkında',
-      { selector: '.top-content .text-content:nth-of-type(2) h4' }
-    ))
-  })
-  test('[4] ana içerik paragrafları doğru metinleri içeriyor', () => {
-    expect(screen.getByText(
-      /Özellikler içeriği elementum magna eros/,
-      { selector: '.top-content .text-content p' }
-    ))
-    expect(screen.getByText(
-      /Hakkında içeriği elementum magna eros/,
-      { selector: '.top-content .text-content p' }
-    ))
-  })
-  test('[5] aşağıdaki başlıklar doğru kelimeleri içeriyor', () => {
-    expect(screen.getByText(
-      'Servisler',
-      { selector: '.bottom-content .text-content h4' }
-    ))
-    expect(screen.getByText(
-      'Ürünler',
-      { selector: '.bottom-content .text-content h4' }
-    ))
-    expect(screen.getByText(
-      'Vizyon',
-      { selector: '.bottom-content .text-content h4' }
-    ))
-  })
-  test('[6] bottom content headings contain the correct text', () => {
-    expect(screen.getByText(
-      /Servisler içeriği elementum magna eros/,
-      { selector: '.bottom-content .text-content p' }
-    ))
-    expect(screen.getByText(
-      /Ürünler içeriği elementum magna eros/,
-      { selector: '.bottom-content .text-content p' }
-    ))
-    expect(screen.getByText(
-      /Vizyon içeriği elementum magna eros/,
-      { selector: '.bottom-content .text-content p' }
-    ))
-  })
-  test('[7] iletişim bilgileri doğru kelimeleri içeriyor', () => {
-    expect(screen.getByText(
-      'İletişim',
-      { selector: 'section.contact h4' }
-    ))
-    expect(screen.getByText(
-      '100 numara Bilmem ne sokak Ankara\'da bir semt, Türkiye',
-      { selector: 'section.contact p' }
-    ))
-    expect(screen.getByText('+90 (123) 456-7899',
-      { selector: 'section.contact p' }
-    ))
-    expect(screen.getByText(
-      'satis@birsirketsitesi.com.tr',
-      { selector: 'section.contact p' }
-    ))
-  })
-  test('[8] copyright bilgisi doğru kelimeleri içeriyor', () => {
-    expect(screen.getByText(
-      /Copyright Bir Şirket Sitesi 2022/,
-      { selector: 'footer a' }
-    ))
-  })
-})
-describe('img sources', () => {
-  test('[9] üstteki logo img src si doğru', () => {
-    expect(screen.getByAltText(/Bir Şirket Sitesi logo/))
-      .toHaveAttribute('src', 'http://localhost:9000/img/logo.png')
-  })
-  test('[10] cta img src si doğru', () => {
-    expect(screen.getByAltText(/Bir kod parçası resmi/))
-      .toHaveAttribute('src', 'http://localhost:9000/img/cta.png')
-  })
-  test('[11] ortadaki img doğru src ye sahip', () => {
-    expect(screen.getByAltText(/Ekran boyunca bir kod parçası resmi/))
-      .toHaveAttribute('src', 'http://localhost:9000/img/accent.png')
-  })
-})
-describe('class isimleri', () => {
-  test('[12] menü linklerinin class\'ı italic', () => {
+describe("metin içerikleri", () => {
+  test("[1] header menü linkleri doğru kelimeleri içeriyor", () => {
+    expect(screen.getByText("Servisler", { selector: "nav a" }));
+    expect(screen.getByText("Ürünler", { selector: "nav a" }));
+    expect(screen.getByText("Vizyon", { selector: "nav a" }));
+    expect(screen.getByText("Özellikler", { selector: "nav a" }));
+    expect(screen.getByText("Hakkımızda", { selector: "nav a" }));
+    expect(screen.getByText("İletişim", { selector: "nav a" }));
+  });
+  test("[2] cta elementleri doğru kelimeleri içeriyor", () => {
+    expect(screen.getByText("Bu DOM Mükemmel"));
+    expect(screen.getByText("Başlayın"));
+  });
+  test("[3] ana içerik başlığı doğru kelimeyi içeriyor", () => {
+    expect(
+      screen.getByText("Özellikler", {
+        selector: ".top-content .text-content:nth-of-type(1) h4",
+      })
+    );
+    expect(
+      screen.getByText("Hakkında", {
+        selector: ".top-content .text-content:nth-of-type(2) h4",
+      })
+    );
+  });
+  test("[4] ana içerik paragrafları doğru metinleri içeriyor", () => {
+    expect(
+      screen.getByText(/Özellikler içeriği elementum magna eros/, {
+        selector: ".top-content .text-content p",
+      })
+    );
+    expect(
+      screen.getByText(/Hakkında içeriği elementum magna eros/, {
+        selector: ".top-content .text-content p",
+      })
+    );
+  });
+  test("[5] aşağıdaki başlıklar doğru kelimeleri içeriyor", () => {
+    expect(
+      screen.getByText("Servisler", {
+        selector: ".bottom-content .text-content h4",
+      })
+    );
+    expect(
+      screen.getByText("Ürünler", {
+        selector: ".bottom-content .text-content h4",
+      })
+    );
+    expect(
+      screen.getByText("Vizyon", {
+        selector: ".bottom-content .text-content h4",
+      })
+    );
+  });
+  test("[6] bottom content headings contain the correct text", () => {
+    expect(
+      screen.getByText(/Servisler içeriği elementum magna eros/, {
+        selector: ".bottom-content .text-content p",
+      })
+    );
+    expect(
+      screen.getByText(/Ürünler içeriği elementum magna eros/, {
+        selector: ".bottom-content .text-content p",
+      })
+    );
+    expect(
+      screen.getByText(/Vizyon içeriği elementum magna eros/, {
+        selector: ".bottom-content .text-content p",
+      })
+    );
+  });
+  test("[7] iletişim bilgileri doğru kelimeleri içeriyor", () => {
+    expect(screen.getByText("İletişim", { selector: "section.contact h4" }));
+    expect(
+      screen.getByText(
+        "100 numara Bilmem ne sokak Ankara'da bir semt, Türkiye",
+        { selector: "section.contact p" }
+      )
+    );
+    expect(
+      screen.getByText("+90 (123) 456-7899", { selector: "section.contact p" })
+    );
+    expect(
+      screen.getByText("satis@birsirketsitesi.com.tr", {
+        selector: "section.contact p",
+      })
+    );
+  });
+  test("[8] copyright bilgisi doğru kelimeleri içeriyor", () => {
+    expect(
+      screen.getByText(/Copyright Bir Şirket Sitesi 2022/, {
+        selector: "footer a",
+      })
+    );
+  });
+});
+describe("img sources", () => {
+  test("[9] üstteki logo img src si doğru", () => {
+    expect(screen.getByAltText(/Bir Şirket Sitesi logo/)).toHaveAttribute(
+      "src",
+      "http://localhost:9000/img/logo.png"
+    );
+  });
+  test("[10] cta img src si doğru", () => {
+    expect(screen.getByAltText(/Bir kod parçası resmi/)).toHaveAttribute(
+      "src",
+      "http://localhost:9000/img/cta.png"
+    );
+  });
+  test("[11] ortadaki img doğru src ye sahip", () => {
+    expect(
+      screen.getByAltText(/Ekran boyunca bir kod parçası resmi/)
+    ).toHaveAttribute("src", "http://localhost:9000/img/accent.png");
+  });
+});
+describe("class isimleri", () => {
+  test("[12] menü linklerinin class'ı italic", () => {
     // screen.getAllByText(/[\s\S]*/, { selector: 'nav a' }) // TODO: research which
-    for (let link of document.querySelectorAll('nav a')) {
-      expect(link).toHaveAttribute('class', 'italic')
+    for (let link of document.querySelectorAll("nav a")) {
+      expect(link).toHaveAttribute("class", "italic");
     }
-  })
-  test('[13] class\'ı italic olan başka link yok', () => {
-    expect(document.querySelector('footer a')).not.toHaveAttribute('class', 'italic')
-  })
-  test('[14] footerdaki linkin class\'ı bold', () => {
-    const link = document.querySelector('footer a')
-    expect(link).toHaveAttribute('class', 'bold')
-  })
-  test('[15] class\'ı bold olan başka link yok', () => {
-    for (let link of document.querySelectorAll('nav a')) {
-      expect(link).not.toHaveAttribute('class', 'bold')
+  });
+  test("[13] class'ı italic olan başka link yok", () => {
+    expect(document.querySelector("footer a")).not.toHaveAttribute(
+      "class",
+      "italic"
+    );
+  });
+  test("[14] footerdaki linkin class'ı bold", () => {
+    const link = document.querySelector("footer a");
+    expect(link).toHaveAttribute("class", "bold");
+  });
+  test("[15] class'ı bold olan başka link yok", () => {
+    for (let link of document.querySelectorAll("nav a")) {
+      expect(link).not.toHaveAttribute("class", "bold");
     }
-  })
-})
+  });
+});
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-const siteContent = { // BU NESNEYİ DEĞİŞTİRMEYİN
-  "nav": {
+const siteContent = {
+  // BU NESNEYİ DEĞİŞTİRMEYİN
+  nav: {
     "nav-item-1": "Servisler",
     "nav-item-2": "Ürünler",
     "nav-item-3": "Vizyon",
@@ -7,39 +8,78 @@ const siteContent = { // BU NESNEYİ DEĞİŞTİRMEYİN
     "nav-item-5": "Hakkımızda",
     "nav-item-6": "İletişim",
   },
-  "cta": {
-    "h1": "Bu DOM Mükemmel",
-    "button": "Başlayın",
+  cta: {
+    h1: "Bu DOM Mükemmel",
+    button: "Başlayın",
   },
   "ana-içerik": {
     "özellikler-h4": "Özellikler",
-    "özellikler-içerik": "Özellikler içeriği elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "özellikler-içerik":
+      "Özellikler içeriği elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
     "hakkımızda-h4": "Hakkında",
-    "hakkımızda-içerik": "Hakkında içeriği elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "hakkımızda-içerik":
+      "Hakkında içeriği elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
     "servisler-h4": "Servisler",
-    "servisler-içeriği": "Servisler içeriği elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "servisler-içeriği":
+      "Servisler içeriği elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
     "ürünler-h4": "Ürünler",
-    "ürünler-içeriği": "Ürünler içeriği elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "ürünler-içeriği":
+      "Ürünler içeriği elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
     "vizyon-h4": "Vizyon",
-    "vizyon-içeriği": "Vizyon içeriği elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "vizyon-içeriği":
+      "Vizyon içeriği elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
   },
-  "iletisim": {
+  iletisim: {
     "iletişim-h4": "İletişim",
-    "adres": "100 numara Bilmem ne sokak Ankara'da bir semt, Türkiye",
-    "telefon": "+90 (123) 456-7899",
-    "email": "satis@birsirketsitesi.com.tr",
+    adres: "100 numara Bilmem ne sokak Ankara'da bir semt, Türkiye",
+    telefon: "+90 (123) 456-7899",
+    email: "satis@birsirketsitesi.com.tr",
   },
-  "footer": {
-    "copyright": "Copyright Bir Şirket Sitesi 2022",
+  footer: {
+    copyright: "Copyright Bir Şirket Sitesi 2022",
   },
-  "images": {
+  images: {
     "logo-img": "http://localhost:9000/img/logo.png",
     "cta-img": "http://localhost:9000/img/cta.png",
     "accent-img": "http://localhost:9000/img/accent.png",
   },
 };
 
-console.log('Proje açıldı!')
-
+console.log("Proje açıldı!");
 
 /* Kodlar Buradan aşağıya */
+
+const links = document.querySelectorAll("nav a");
+const navKeys = Object.keys(siteContent.nav);
+for (let i = 0; i < navKeys.length; i++) {
+  links[i].textContent = siteContent.nav[navKeys[i]];
+  links[i].classList.add("italic");
+}
+
+const myImages = document.querySelectorAll("img");
+const logoKeys = Object.keys(siteContent.images);
+for (let i = 0; i < logoKeys.length; i++) {
+  myImages[i].src = siteContent.images[logoKeys[i]];
+}
+
+const myCta = document.querySelectorAll(".cta-text h1, .cta-text button");
+const ctaKeys = Object.keys(siteContent.cta);
+for (let i = 0; i < ctaKeys.length; i++) {
+  myCta[i].textContent = siteContent.cta[ctaKeys[i]];
+}
+
+const icerik = document.querySelectorAll(".main-content h4, .main-content p");
+const icerikKeys = Object.keys(siteContent["ana-içerik"]);
+for (let i = 0; i < icerikKeys.length; i++) {
+  icerik[i].textContent = siteContent["ana-içerik"][icerikKeys[i]];
+}
+
+const contacts = document.querySelectorAll(".contact h4, .contact p");
+const contactKeys = Object.keys(siteContent.iletisim);
+for (let i = 0; i < contactKeys.length; i++) {
+  contacts[i].textContent = siteContent.iletisim[contactKeys[i]];
+}
+
+const bold = document.querySelector("footer a");
+bold.textContent = siteContent.footer.copyright;
+bold.classList.add("bold");
